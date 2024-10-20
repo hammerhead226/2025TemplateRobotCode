6d4291098468597d6f4589305e51f251c1369eaf
Pranav Maringanti <141516835+nootsquared@users.noreply.github.com>
2024-10-20 14:10:28 -0400

    Initial Commit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2025TemplateRobotCode";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "9254cce10bfabd81a002298347485c8c8798c7a6";
+  public static final String GIT_DATE = "2024-10-20 14:05:22 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-10-20 14:08:26 EDT";
+  public static final long BUILD_UNIX_TIME = 1729447706293L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
